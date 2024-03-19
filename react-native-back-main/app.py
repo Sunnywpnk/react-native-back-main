5700fbcb167ef77d0fb8617249d71c20333be7ab
Sunny <wichayaporn.pu@ku.th>
2024-03-19 23:22:57 +0700

    first commit

---
+++ react-native-back-main/app.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, jsonify, request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+books=[
+    {"id":1,"title":"Book 1","author":"Author 1"},
+    {"id":2,"title":"Book 2","author":"Author 2"},
+    {"id":3,"title":"Book 3","author":"Author 3"}
+]
+
+@app.route("/")
+def hello_world():
+    return "<h1>hello_world<h1/>"
+
+@app.route("/books",methods=["GET"])
+@cross_origin()
+def get_all_books():
+    return jsonify({"books":books})
+
+@app.route("/books",methods=["POST"])
+@cross_origin()
+def insert_id_book():
+    new_book = request.get_json()
+    new_book['id'] = int(new_book['id'])
+    books.append(new_book)
+    for book in books :
+        print(book)
+    return jsonify({"books": books})
+
+@app.route("/books/<int:data_book>", methods=["PUT"])
+@cross_origin()
+def put_id_book(data_book):
+    for book in books:
+        if book["id"] == data_book:
+            updated_book = request.get_json()
+            book.update(updated_book)
+            return jsonify({"books": books}), 200
+
+    return jsonify({"error": "Book not found"}), 404
+
+@app.route("/books/<int:data_book>", methods=["DELETE"])
+@cross_origin()
+def delete_id_book(data_book):
+    for book in books:
+        if book["id"] == data_book:
+            books.remove(book)
+            return jsonify({"books": books}), 200
+    return jsonify({"error": "Book not found"}), 404
+
+if __name__ == "__main__":  
+    app.run(host="0.0.0.0",port=5001,debug=True)
